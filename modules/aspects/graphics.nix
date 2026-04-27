6de5a0d434fbba5af9d4bb6ff8a09786c7a52cdd
alice <paul-pexe@proton.me>
2026-04-27 22:11:05 +0200

    cuda

--- modules/aspects/graphics.nix
+++ modules/aspects/graphics.nix
@@ -1,8 +1,20 @@
 { den, ... }: {
   den.aspects.graphics = {
     nixos = { pkgs, ... } : {
+      nix.settings = {
+        substituters = [
+          "https://cache.nixos-cuda.org"
+        ];
+        trusted-public-keys = [
+          "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+        ];
+      };
+      #nixpkgs.config.cudaSupport = true;
       environment.systemPackages = with pkgs; [
-        blender
+        (blender.override {
+          config.cudaSupport=true;
+          config.rocmSupport=true;
+        })
       ];
     };
   };
