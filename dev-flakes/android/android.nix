9d6121010bd9fda7ac160bc8ce24d39842f495c9
alice <alice>
2026-05-03 22:35:54 +0200

    added android dev flake

---
+++ dev-flakes/android/android.nix
@@ -0,0 +1,25 @@
+{
+  description = "Android dev flake template";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs @ {self, ...}: inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+    systems = [
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    perSystem = {pkgs, ...}: {
+      devShells = {
+        default = let 
+          androidTools = pkgs.android-tools;
+        in pkgs.mkShell {
+          nativeBuildInputs = [
+            androidTools
+          ];
+        };
+      };
+    };
+  };
+}
